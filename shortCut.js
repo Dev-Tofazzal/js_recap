bf3e2fcd9a966f92eb5891243fa4aae6abbf50ff
mdtofazzalislam <dev.tofazzal122@gmail.com>
2025-05-03 21:55:42 +0600

    storage

---
+++ shortCut.js
@@ -0,0 +1,15 @@
+// "almas",5,true , {} , []
+// "",0,false, null, undefined
+
+
+let myVar = 5;
+if(myVar){
+    myVar = myVar*100;
+}
+
+else{
+    myVar = 0 
+}
+
+const number = 20 + 20 + "" + 20 + 20;
+console.log(number);
